9f52d8941d4e90fbc4c61526b46d7f2fce010636
Carlos Emanuel <devcarlos0112@gmail.com>
2025-05-05 15:29:47 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#c542f5'
+      }
+    },
   },
   plugins: [],
 }
